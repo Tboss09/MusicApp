429cc25db4cdb18096c36867c0b95120004c5062
TonyMike <tee.jhay1@gmail.com>
2021-06-27 06:42:30 -0700

    Update:Pushed some content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Footer from './components/footer/Footer'
+
+// internal files
+import Nav from './components/navigation/Nav'
+import Home from './components/home/Home'
+import { Box } from "@chakra-ui/react";
+
 
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <>
+      <Nav/>
+      <Home/>
+      <Footer />
+    </>
   );
 }
 export default App;
